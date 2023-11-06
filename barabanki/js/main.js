d2e8a18d4f45bdb0123c13bc04d48a9255d0e142
VedantCMS <vedantgupta.cms@gmail.com>
2023-11-06 12:02:36 +0530

    pushed

--- barabanki/js/main.js
+++ barabanki/js/main.js
@@ -17,3 +17,45 @@ jQuery(function () {
 window.addEventListener('load', function () {
   document.getElementById('loader').style.display = 'none';
 });
+
+
+jQuery(document).ready(function () {
+  jQuery(".signup-form").hide();
+
+  jQuery("#login").click(function () {
+    jQuery(".login-form").hide();
+    jQuery(".signup-form").show();
+  });
+
+  jQuery("#signup").click(function () {
+    jQuery(".signup-form").hide();
+    jQuery(".login-form").show();
+  });
+});
+
+jQuery(document).ready(function () {
+  jQuery("#verify-number").hide();
+  jQuery("#verify-otp").hide();
+  jQuery("#otp-resent").hide();
+
+  jQuery("#send-otp").click(function () {
+    jQuery("#mobile-number").hide();
+    jQuery("#verify-number").show();
+    jQuery("#send-otp").hide();
+    jQuery("#verify-otp").show();
+  });
+
+  jQuery("#backtonumber").click(function () {
+    jQuery("#verify-number").hide();
+    jQuery("#mobile-number").show();
+    jQuery("#verify-otp").hide();
+    jQuery("#send-otp").show();
+  });
+
+  jQuery('#resend-otp').click(function () {
+    jQuery("#otp-resent").fadeIn("slow");
+    setTimeout(function () {
+      jQuery("#otp-resent").fadeOut("slow");
+    }, 5000);
+  });
+});
