88e57a3ae323e4db2c926a9b720c3d049120c545
VedantCMS <vedantgupta.cms@gmail.com>
2023-11-29 17:39:39 +0530

    owlpushed

--- balaganj/js/main.js
+++ balaganj/js/main.js
@@ -1,76 +1,19 @@
-jQuery(window).scroll(function () {
-  if (jQuery(this).scrollTop() > 45) {
-    jQuery("header").addClass("fixed-header");
-  } else {
-    jQuery("header").removeClass("fixed-header");
-  }
-});
+document.addEventListener("DOMContentLoaded", function () {
 
-// Navbar 
+  window.addEventListener('scroll', function () {
 
-(function ($) {
-  $(function () {
-
-    $('#navbar-toggle').click(function () {
-      $('nav ul').slideToggle();
-    });
-
-    $('#navbar-toggle').on('click', function () {
-      this.classList.toggle('active');
-    });
-
-    $('nav ul li a:not(:only-child)').click(function (e) {
-      $(this).siblings('.navbar-dropdown').slideToggle("slow");
-
-      $('.navbar-dropdown').not($(this).siblings()).hide("slow");
-      e.stopPropagation();
-    });
-
-    $('html').click(function () {
-      $('.navbar-dropdown').hide();
-    });
-  });
-})(jQuery);
-
-
-$('.owl-carousel').owlCarousel({
-  loop: true,
-  margin: 30,
-  nav: false,
-  autoplay: 3000,
-  dots: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 2
-    },
-    1000: {
-      items: 3
+    if (window.scrollY > 300) {
+      document.getElementById('navbar_top').classList.add('fixed-top');
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector('.navbar').offsetHeight;
+      document.body.style.paddingTop = navbar_height + 'px';
+    } else {
+      document.getElementById('navbar_top').classList.remove('fixed-top');
+      // remove padding top from body
+      document.body.style.paddingTop = '0';
     }
-  }
-})
-
-// Including Header Footer
-
-// function loadContent(file, elementId) {
-//   var xhr = new XMLHttpRequest();
-//   xhr.onreadystatechange = function () {
-//     if (xhr.readyState == 4 && xhr.status == 200) {
-//       document.getElementById(elementId).innerHTML = xhr.responseText;
-//     }
-//   };
-//   xhr.open("GET", file, true);
-//   xhr.send();
-// }
-
-// loadContent("include/header.html", "header");
-// loadContent("include/footer.html", "footer");
-
-// loadContent("../include/campus-header.html", "campus-header");
-// loadContent("../include/campus-footer.html", "campus-footer");
-
+  });
+});
 
 jQuery(function () {
   jQuery("#header").load("include/header.html");
@@ -82,3 +25,21 @@ jQuery(function () {
 window.addEventListener('load', function () {
   document.getElementById('loader').style.display = 'none';
 });
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  animateOut: 'fadeOut',
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+})
