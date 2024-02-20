f9b7914b97b872c165ad1ba5885886c6ce7c05c5
VedantCMS <vedantgupta.cms@gmail.com>
2024-02-20 09:51:31 +0530

    df

--- cis/js/main.js
+++ cis/js/main.js
@@ -1,21 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-  window.addEventListener('scroll', function () {
-
-    if (window.scrollY > 200) {
-      document.getElementById('navbar_top').classList.add('fixed-top');
-      // add padding top to show content behind navbar
-      navbar_height = document.querySelector('.navbar').offsetHeight;
-      document.body.style.paddingTop = navbar_height + 'px';
-    } else {
-      document.getElementById('navbar_top').classList.remove('fixed-top');
-      // remove padding top from body
-      document.body.style.paddingTop = '0';
-    }
-  });
+jQuery(window).scroll(function () {
+  if (jQuery(this).scrollTop() > 45) {
+    jQuery("header").addClass("fixed-header");
+  } else {
+    jQuery("header").removeClass("fixed-header");
+  }
 });
 
-// Header JS Ends
 
 jQuery(document).ready(function () {
   jQuery(".glimpse-carousel").owlCarousel();
